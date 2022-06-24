88921cd99f06998f8f869dbc1352c43f93d58953
Coder-investor <70206474+Coder-investor@users.noreply.github.com>
2022-06-24 20:22:18 +0000

    Added comments to explain what the code is doing

--- day_1.py
+++ day_1.py
@@ -1,4 +1,7 @@
 print("Band Name Generator")
+# The first input is where the user is from. This input is stored in the variable called input_place.
 input_place = input("Enter the name of the town or city where you were born: ")
+# The second input is the name of their first pet. This input is stored in a variable called input_pet.
 input_pet = input("Enter the name of your first pet: ")
+# The two variables are then concatenated together. This generates a band name.
 print("Your Band Name is: " + input_place + " " + input_pet)
