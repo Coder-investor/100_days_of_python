c0eda582209cf28dbffe6305fbe8b46e7eb9f053
Coder-investor <70206474+Coder-investor@users.noreply.github.com>
2022-06-24 20:15:27 +0000

    Created a py srcipt to generate a band name

---
+++ day_1.py
@@ -0,0 +1,4 @@
+print("Band Name Generator")
+input_place = input("Enter the name of the town or city where you were born: ")
+input_pet = input("Enter the name of your first pet: ")
+print("Your Band Name is: " + input_place + " " + input_pet)
